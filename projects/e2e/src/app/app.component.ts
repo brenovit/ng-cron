5a7f9844289db27925a904b8a3766b57fcbb11fa
Breno Nunes Arantes <breno.arantes97@gmail.com>
2026-03-20 23:11:02 +0100

    Upgrade to angular 19 standalone

--- projects/e2e/src/app/app.component.ts
+++ projects/e2e/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-root',
+    templateUrl: './app.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: false
 })
 export class AppComponent {}
